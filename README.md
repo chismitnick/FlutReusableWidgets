6b352676646af775b07132ac19e3814f29534cc1
csamiselo <chisambwe.samiselo@gmail.com>
2020-07-02 17:16:50 +0200

    Initial commit for reusable widget library

---
+++ README.md
@@ -0,0 +1,13 @@
+# flutterreusewidgets
+Application made using reusable widgets
+Ultimate goal is to create a library of custom widgets that 
+can imported into any project 
+
+## Getting Started
+
+
+
+
+
+
+
---
+++ lib/Utils/Utils.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'colors.dart';
+
+//The Utils class define individual widgets
+
+class Utils {
+  static Future<bool> displayToast(String message) {
+    return Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 2,
+        backgroundColor: primaryColor,
+        textColor: Colors.white,
+        fontSize: 14.0);
+  }
+
+  static SnackBar displaySnackBar(String message,
+      {String actionMessage, VoidCallback onClick}) {
+    return SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white, fontSize: 14.0),
+      ),
+
+      action: (actionMessage != null)
+          ? SnackBarAction(
+        textColor: Colors.white,
+        label: actionMessage,
+        onPressed: () {
+          return onClick();
+        },
+      )
+          : null,
+      duration: Duration(seconds: 5),
+      backgroundColor: primaryColor,
+    );
+  }
+}
